ccffcf851c3f61deb204dd232feee04b16153b82
sunbaoxu <1194063234@qq.com>
2019-07-31 11:37:56 +0800

    初步引入 photo-sphere-viewer

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,139 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+
+.PSViewer{
+  overflow : hidden;
+  display : block;
+  width : 100%;
+  height : inherit;
+  position : relative;
+  .fix{
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: 0 auto;
+    width: 200px;
+  }
+  .psv-hud{
+    width: 100%;
+    height: 100%;
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 35px;
+    z-index: 10;
+    svg{
+      display: block;
+      width: 100%;
+      height: 100%
+    }
+  }
+  .psv-container{
+    display: block;
+    width: 100%;
+    height: inherit;
+    .psv-loader-container{
+      width: 50px;
+      height: 50px;
+      margin: 0 auto;
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 50%;
+      margin-top: -25px;
+      *{
+        max-width: 100%;
+        max-height: 100%;
+      }
+    }
+  }
+  .psv-canvas-container{
+    display: block;
+    width: 100%;
+    height: inherit;
+    canvas{
+      display: block;
+      width: 100%;
+      height: inherit;
+    }
+  }
+  .psv-navbar{
+    &.psv-navbar--open{
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      z-index: 60;
+      padding: 8px 0;
+      display: block;
+      background-color: rgba(255,255,255,0.7);
+      .psv-caption{
+        display: inline-block;
+      }
+      .psv-button{
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+        float: left;
+        &.psv-zoom-button{
+          float: left;
+          width: auto;
+          .psv-zoom-button-minus{
+            width: 20px;
+            float: left;
+            height: 20px;
+            background-color: transparent;
+            line-height: 20px;
+            color: rgba(255, 255, 255, 0.7);
+            cursor: pointer;
+          }
+          .psv-zoom-button-range{
+            float: left;
+            padding: 10px 8px 0 8px;
+            background-color: transparent;
+            cursor: pointer;
+            .psv-zoom-button-line{
+              width: 50px;
+              height: 2px;
+              background-color: rgba(0, 0, 0, 0.7);
+              position: relative;
+              .psv-zoom-button-handle{
+                position: absolute;
+                top: -3px;
+                left: -3.5px;
+                width: 8px;
+                height: 8px;
+                border-radius: 50%;
+                background-color: rgba(0, 0, 0, 0.7);
+              }
+            }
+          }
+          .psv-zoom-button-plus{
+            width: 20px;
+            float: left;
+            height: 20px;
+            background-color: transparent;
+            line-height: 20px;
+            color: rgba(255, 255, 255, 0.7);
+            cursor: pointer;
+          }
+        }
+        &.psv-autorotate-button{
+          margin-right: 8px;
+          margin-left: 8px;
+        }
+        &.psv-fullscreen-button{
+          float:right;
+          margin-right: 8px;
+        }
+      }
+      .psv-caption{
+        height: inherit;
+      }
     }
   }
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,64 @@
 <template>
   <div class="hello">
-    sdaf
+    <div class="PSViewer" ref="psvdbg"></div>
+    <!-- <img :src="img" alt=""> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { Getter, Mutation } from 'vuex-class'
+// import PhotoSphereViewer from 'photo-sphere-viewer';
+var THREE   = require('three');
+var PhotoSphereViewer   = require('photo-sphere-viewer');
+// import THREE from 'three'
+
+
 @Component
 export default class HelloWorld extends Vue {
   @Getter private orgNo :any;
+  width=798;
+  height=498;
+  img='head-back.png';
+
+   init () {
+    const self   = this;
+
+    var PSV = new PhotoSphereViewer({
+      // Panorama, given in base 64
+      //必填参数，全景图的路径。
+      panorama: self.img,
+
+      // Container
+      container: self.$refs.psvdbg,
+
+      // Deactivate the animation
+      time_anim: false,
+
+      // Display the navigation bar
+      // navbar: true,
+      navbar:[
+        'autorotate',
+        'zoom',
+        'fullscreen'
+      ],
+      navbar_style:{
+        backgroundColor:'rgba(58,67,77,0.7)'
+      },
+
+      // Resize the panorama
+      size: {
+        width: '100%',
+        height: '498px'
+      }
+    });
+        }
 
   mounted(){
-    console.log(this.orgNo,'asdf');
+    this.init();
+    // console.log(this.orgNo,'asdf');
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
